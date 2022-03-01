300a7105181f9868168fcea87c13b84e5362c3db
Andile Selotso <mellismith72@gmail.com>
2022-03-01 10:36:57 -0500

    working slightly more pleasing to look at

--- return_the_larger_number/return_largest.py
+++ return_the_larger_number/return_largest.py
@@ -1,9 +1,7 @@
 
 def return_larger_number(num_one, num_two):
     if num_one >= num_two: 
-        return  num_one
-
-    elif num_two >= num_one:
-        return  num_two 
+        return num_one
+    return num_two 
 
 
