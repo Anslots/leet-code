57f2639d9707b809ce905862eea46453f64ee41f
Andile Selotso <mellismith72@gmail.com>
2022-03-01 16:16:30 -0500

    finding max in array

--- find_max_in_array/main.py
+++ find_max_in_array/main.py
@@ -1,4 +1,3 @@
-
 '''
 function findMax() takes in a list li
 it should then return the maximum number in that list
@@ -6,7 +5,15 @@ it should then return the maximum number in that list
 Examples:
 [1, 6, 12, 7] return 12
 [2, 17, 8, 7] return 17
+[1, 1, 1, 2, 1, 1] return 2
+[-3, -4, -2] return -2
+[-15, -13, -27] return -13
 '''
 def findMax(li):
-    # fill in logic here
-    return 0
+    maxim = li[0]
+    for i in li:
+        if i > maxim:
+            maxim = i
+    return maxim
+
+
--- find_max_in_array/test_main.py
+++ find_max_in_array/test_main.py
@@ -6,3 +6,13 @@ def test_gets_maximum_8():
 
 def test_gets_maximum_6():
     assert findMax([1, 6, 2, 3]) == 6
+
+
+def test_gets_maximum_2():
+    assert findMax([1, 1, 1, 2, 1, 1, 1]) == 2
+
+def test_gets_maximum_negative2():
+    assert findMax([-3, -4, -2]) == -2
+
+def test_gets_maximum_negative_large():
+    assert findMax([-3456, -4423, -2687]) == -2687
